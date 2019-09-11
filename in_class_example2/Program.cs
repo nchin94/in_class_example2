c5a9c11fb6117ff5737d13a2baa60636529be540
nickchin.1994@gmail.com <nickchin.1994@gmail.com>
2019-09-10 20:57:52 -0400

    Add project files.

---
+++ in_class_example2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace in_class_example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many years experience do you have in professional programming:");
+            int years = Convert.ToInt32(Console.ReadLine());
+            int index = 0;
+
+            while (index <= years)
+            {
+                Console.WriteLine("You have " +index + " of experience");
+                index++;
+            }
+             
+        }
+    }
+}
